6887260fca2bb0276b7d60def191fddda8caf71e
Dmitry Gutov <dgutov@yandex.ru>
2012-07-05 04:32:46 +0400

    Update comments and docstrings; separate private functions

--- point-stack.el
+++ point-stack.el
@@ -1,62 +1,84 @@
-;; -- POINT STACK -----------------------------------------------------------------------
-;; matt harrison (matthewharrison@gmail.com)
-;; dmitry gutov  (dgutov@yandex.ru)
-;;
-;; Provides forward/back stack for point.  I use load it like so:
-;;
-;; (add-to-list 'load-path "/home/matt/work/emacs/point-stack")
-;; (require 'point-stack)
-;; (global-set-key '[(f5)] 'point-stack-push)
-;; (global-set-key '[(f6)] 'point-stack-pop)
-;; (global-set-key '[(f7)] 'point-stack-forward-stack-pop)
-;;
-;; Then when I know I'm going to want to come back to where I am I hit
-;; f5.  This stores the location of of the point.  When I want to come
-;; back to that point hit f6.  I can go forward by hitting f7.
-;;
-;; based on http://www.emacswiki.org/emacs/JohnConnors
-;; enhanced with forward stack and made both local to the current window
+;;; point-stack.el --- Back and forward stacks for point location
 
+;; Author: Dmitry Gutov <dgutov@yandex.ru>
+;; Version: 1.0
+
+;;; Commentary:
+
+;; Provides window-local back and forward stacks for point.
+;; Each stack value contains buffer reference, point position,
+;; and window scroll position.
+;; Both stacks are local to current window, in other words, stacks in
+;; different windows are not related.
+;;
+;; To navigate the stacks, bind back and forward commands to some keys:
+;; (global-set-key (kbd "C-M-,") 'point-stack-pop)
+;; (global-set-key (kbd "C-M-.") 'point-stack-forward-stack-pop)
+;;
+;; The recommended usage is to save locations automatically on navigation:
+;; (point-stack-setup-advices)
+;;
+;; To push values to stack manually instead, bind the push command:
+;; (global-set-key [f5] 'point-stack-push)
+;;
+;; Based on https://github.com/mattharrison/point-stack
+;; which is in turn based on http://www.emacswiki.org/emacs/JohnConnors
+
+;;; Code:
+
+(defgroup point-stack nil
+  "Back and forward stacks for point location")
+
+(defcustom point-stack-advised-functions
+  '(isearch-mode find-function find-library
+    find-variable find-face-definition imenu
+    helm-c-etags-default-action)
+  "Functions that will be advised by `point-stack-setup-advices'."
+  :group 'point-stack)
+
+;;;###autoload
 (defun point-stack-push ()
-  "Push current buffer, point, and scroll position onto stack."
+  "Push current buffer, point, and window scroll position onto the stack."
   (interactive)
-  (point-stack-store 'stack)
-  (point-stack-value 'forward 'set nil) ; new step resets forward history
-  (message "Location marked."))
+  (point-stack--store 'stack)
+  (point-stack--value 'forward 'set nil) ; New step resets forward history.
+  (message "Location saved"))
 
+;;;###autoload
 (defun point-stack-pop ()
-  "Push current location onto forward stack, move to previous location."
+  "Push current location on forward stack, move to previous location."
   (interactive)
-  (if (point-stack-value 'stack 'null)
-      (message "Stack is empty.")
-    (point-stack-store 'forward)
-    (point-stack-go 'stack)
-    (point-stack-value 'stack 'shift)))
+  (if (point-stack--value 'stack 'null)
+      (message "Stack is empty")
+    (point-stack--store 'forward)
+    (point-stack--go 'stack)
+    (point-stack--value 'stack 'shift)))
 
+;;;###autoload
 (defun point-stack-forward-stack-pop ()
-  "Push current location onto stack, pop and move to location from forward stack."
+  "Push current location on stack, pop and move to location from forward stack."
   (interactive)
-  (if (point-stack-value 'forward 'null)
-      (message "forward Stack is empty.")
-    (point-stack-store 'stack)
-    (point-stack-go 'forward)
-    (point-stack-value 'forward 'shift)))
+  (if (point-stack--value 'forward 'null)
+      (message "Forward stack is empty")
+    (point-stack--store 'stack)
+    (point-stack--go 'forward)
+    (point-stack--value 'forward 'shift)))
 
-(defun point-stack-store (stack)
-  (let ((loc (point-stack-value stack 'car)))
-    ;; don't push the same location twice
+(defun point-stack--store (stack)
+  (let ((loc (point-stack--value stack 'car)))
+    ;; Don't push the same location twice.
     (unless (and (eq (current-buffer) (car loc))
                  (eq (point) (cadr loc)))
-      (point-stack-value stack 'push
-                         (list (current-buffer) (point) (window-start))))))
+      (point-stack--value stack 'push
+                          (list (current-buffer) (point) (window-start))))))
 
-(defun point-stack-go (stack)
-  (let ((loc (point-stack-value stack 'car)))
+(defun point-stack--go (stack)
+  (let ((loc (point-stack--value stack 'car)))
     (switch-to-buffer (car loc))
     (set-window-start nil (caddr loc))
     (goto-char (cadr loc))))
 
-(defun point-stack-value (name action &optional arg)
+(defun point-stack--value (name action &optional arg)
   (let* ((parameter (intern (concat "point-stack-" (symbol-name name))))
          (value (window-parameter nil parameter)))
     (cond ((eq action 'car)
@@ -69,4 +91,18 @@
                                      ('push (cons arg value))
                                      ('shift (cdr value))))))))
 
+;;;###autoload
+(defun point-stack-setup-advices ()
+  "Advise navigation functions to call `point-stack-push' before
+any navigation is made.  This way, it can be used as a replacement
+for the global mark ring."
+  (mapc (lambda (func)
+          (ad-add-advice
+           func '(point-stack-push nil t (advice . (lambda () (point-stack-push))))
+           'before 'last)
+          (ad-activate func nil))
+        point-stack-advised-functions))
+
 (provide 'point-stack)
+
+;;; point-stack.el ends here
